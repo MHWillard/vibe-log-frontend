ab2cf455ab7c2cea0501ef8f229c051f01dbe6d2
MHWillard <mhwillard@gmail.com>
2024-09-18 11:54:16 -0400

    testing frontend updates

--- src/pages/home-page.tsx
+++ src/pages/home-page.tsx
@@ -2,13 +2,12 @@ import React from "react";
 //import { Auth0Features } from "src/components/auth0-features";
 import { HeroBanner } from "src/components/hero-banner";
 import { PageLayout } from "../components/page-layout";
-import {MainSummary} from "../components/main-page-summary";
+//import {MainSummary} from "../components/main-page-summary";
 
 export const HomePage: React.FC = () => (
   <PageLayout>
     <>
       <HeroBanner />
-      <MainSummary />
     </>
   </PageLayout>
 );
