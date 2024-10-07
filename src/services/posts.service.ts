fb5765e4593dac05d7ed8a2a78a4edc768b858a4
MHWillard <mhwillard@gmail.com>
2024-10-07 15:43:00 -0400

    daily; working on posting functionality

--- src/services/posts.service.ts
+++ src/services/posts.service.ts
@@ -3,11 +3,29 @@ import { ApiResponse } from "../models/api-response";
 import { callExternalApi } from "./api.service";
 
 const apiServerUrl = process.env.REACT_APP_API_SERVER_URL;
+const apiPostUrl = process.env.REACT_APP_API_POST_URL;
 
 export const getPosts = async (): Promise<ApiResponse> => {
   const config: AxiosRequestConfig = {
     url: `${apiServerUrl}`,
     method: "GET",
+    headers: {
+      "content-type": "application/json",
+    },
+  };
+
+  const { data, error } = (await callExternalApi({ config })) as ApiResponse;
+
+  return {
+    data,
+    error,
+  };
+};
+
+export const createPost = async (): Promise<ApiResponse> => {
+  const config: AxiosRequestConfig = {
+    url: `${apiPostUrl}`,
+    method: "POST",
     headers: {
       "content-type": "application/json",
     },
