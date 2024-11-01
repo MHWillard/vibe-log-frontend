9a87160a9c241426f81866dc6a6265f6942271ff
MHWillard <mhwillard@gmail.com>
2024-11-01 11:06:59 -0400

    update frontend; ready for minimal launch

--- src/components/main-page-summary.tsx
+++ src/components/main-page-summary.tsx
@@ -5,7 +5,9 @@ export const MainSummary: React.FC = () => {
 
   return (
     <div className="main-summary-description">
-        <p>Get started today by logging in or signing up for an account.</p>
+        <p>Get started today by logging in:</p><br></br>
+        <p><b>USERNAME:</b> user@vibelogdev.com</p>
+        <p><b>PASSWORD:</b> vibelogin01!</p>
     </div>
   );
 };
