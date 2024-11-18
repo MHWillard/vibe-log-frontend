ab8bf5a5171acba6f96bc0187439fc0964ef185e
MHWillard <mhwillard@gmail.com>
2024-11-18 15:42:52 -0500

    working on emoji posting

--- src/pages/new-post.tsx
+++ src/pages/new-post.tsx
@@ -1,46 +1,16 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { PageLayout } from "../components/page-layout";
 import { createPost } from "../services/posts.service";
 import data from '@emoji-mart/data'
 import Picker from '@emoji-mart/react'
-//import { getUserId } from "../services/users.service";
 
 
 export const NewPost: React.FC = () => {
   //temporary user ID measure: better version will poll with login sessions
   const [postContent, setPostContent] = useState<string>("");
-  //const [userid, setUserid] = useState<string>("");
+  const [currentMood, setCurrentMood] = useState('😄');
   const navigate = useNavigate();
-
-  /*
-  useEffect(() => {
-    let isMounted = true;
-
-    
-    const getMessage = async () => {
-      const { data, error } = await getUserId();
-
-      if (!isMounted) {
-        return;
-      }
-
-      if (data) {
-        setUserid(JSON.stringify(data, null, 2));
-      }
-
-      if (error) {
-        setUserid(JSON.stringify(error, null, 2));
-      }
-    };
-    
-
-    //getMessage();
-
-    return () => {
-      isMounted = false;
-    };
-  }, []);*/
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     event.preventDefault();
@@ -67,6 +37,12 @@ export const NewPost: React.FC = () => {
     }
   };
 
+  const handleClick = (e:any) => {
+    //const selectedMood = e.detail.native
+    console.log(e.detail.unicode);
+    //setCurrentMood(selectedMood);
+  };
+
   return (
     <PageLayout>
       <div className="content-layout">
@@ -79,6 +55,9 @@ export const NewPost: React.FC = () => {
               <strong>What's your vibe? (Posting feature currently a work in progress.)</strong>
             </span>
           </p>
+          <div className="selected-mood">
+            {currentMood}
+          </div>
           <p id="post-form">
             <form onSubmit={handleSubmit}>
               <input id="postInput" type="text" name="content" value={postContent} onChange={handleChange}/><br/>
@@ -86,7 +65,7 @@ export const NewPost: React.FC = () => {
             </form>
           </p>
           <div>
-          <Picker data={data} onEmojiSelect={console.log} />
+          <Picker data={data} onEmojiSelect={handleClick} />
           </div>
         </div>
       </div>
